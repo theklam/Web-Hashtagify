6008036b28b1cc8ea37dbeef39fb00a0a09de953
Kalvin Lam <kalvin.lam5@gmail.com>
2016-10-08 16:05:08 -0400

    Difficult to parse txt file into array

--- index.js
+++ index.js
@@ -72,9 +72,40 @@ function parseResponse(resp) {
   document.getElementById('tags').innerHTML = stringOfRelatedWords;
   //document.getElementById('tags').innerHTML += "<br />Your picture is very, very, very " +
   //document.getElementById('tags').innerHTML.substring(0, document.getElementById('tags').innerHTML.indexOf(",")) + "!"
-  var arrayOfRelatedWords = stringOfRelatedWords.split(",");
+
+  // Reading in list of adjectives into an array
+  // var arrayOfRelatedWords = stringOfRelatedWords.split(",");
+  // var fs = require('fs');
+  // var array = fs.readFileSync('adjectives-list.txt').toString().split("\n");
+  // for(i in array) {
+    // console.log(array[i]);
+  // }
   //console.log(arrayOfRelatedWords);
+  var arr = txtFileToArray("adjectives-list.txt");
+  console.log(arr);
   return stringOfRelatedWords;
+}
+
+// given txt file return array
+function txtFileToArray(txtFile) {
+  $(document).ready(function() {
+    //fetch text file
+    $.get(txtFile, function(data) {
+        //split on new lines
+        var lines = data.split('\n');
+        //create select
+        var dropdown = $('<select>');
+        //iterate over lines of file and create a option element
+        for(var i=0;i<lines.length;i++) {
+            //create option
+            var el = $('<option value="'+i+'">'+lines[i]+'</option>');
+            //append option to select
+            $(dropdown).append(el);
+        }
+        //append select to page
+        $('body').append(dropdown);
+    });
+});
 }
 
 function run(imgurl) {
