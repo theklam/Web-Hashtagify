331821e518e1cbdaaf636705339031cb3f0e2024
Kalvin Lam <kalvin.lam5@gmail.com>
2016-10-08 20:18:42 -0400

    Deleted unnecessary components

--- trianglify.js
+++
@@ -1,7 +0,0 @@
-var trianglify = document.getElementById('trianglify');
-var dimensions = trianglify.getClientRects()[0];
-var pattern = Trianglify({
-    width: dimensions.width,
-    height: dimensions.height
-});
-trianglify.appendChild(pattern.canvas());
